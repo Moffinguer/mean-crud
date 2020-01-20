bd4c5dbb5f6a5dbce5da36d09746da3e8e3ddb2d
Fernando <fernandojosemateos.18@campuscamara.es>
2020-01-20 20:43:55 +0100

    Angular 2.0

--- mean-teachers/frontend/src/app/app.module.ts
+++ mean-teachers/frontend/src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormModule} from '@angular/core';
+import {HttpModule} from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TeachersComponent } from './components/teachers/teachers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeachersComponent
   ],
   imports: [
     BrowserModule,
