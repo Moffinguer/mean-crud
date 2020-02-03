a505864a53e567e629451889666ecceb203e6911
Fernando <fernandojosemateos.18@campuscamara.es>
2020-02-03 19:12:46 +0100

    fin roles

--- mean-teachers/server/controllers/users.controllers.js
+++ mean-teachers/server/controllers/users.controllers.js
@@ -1,7 +1,34 @@
-const User=require("../models/users");
-const bcrypt=require("bcrypt-nodejs");
-const userCtrl={};
+const User = require("../models/users");
+const bcrypt = require("bcrypt-nodejs");
+const userCtrl = {};
 
-userCtrl.saveUser=(req,res)=>{
-    
-}
+userCtrl.saveUser = (req, res) => {
+  const user = new User();
+  var params = req.body;
+  user.name = params.name;
+  user.surname = params.surname;
+  user.email = params.email;
+  user.role = "ROLE_USER"; //NOTA, DEBEMOS CREAR UN USUARIO ADMINISTRADOR
+  user.image = "";
+  if (params.password) {
+    //Encripta y guarda los datos
+    bcrypt.hash(params.password, null, null, (err, hash) => {
+      user.password = hash;
+      if (user.name != null && user.surname != null && user.email != null) {
+        user
+          .save()
+          .then(user => {
+            res.status(200).send({ user: user });
+          })
+          .catch(user => {
+            res.status(500).send({ error: error });
+          });
+      } else {
+        res.status(200).send({ message: "Fill up the form" });
+      }
+    });
+  } else {
+    res.status(200).send({ message: "Enter the password" });
+  }
+};
+module.exports = userCtrl;
--- mean-teachers/server/index.js
+++ mean-teachers/server/index.js
@@ -18,3 +18,16 @@ app.use("/api/users",require("./routers/users.routes"));
 app.listen(app.get("port"), () => {
   console.log("Servidor en el puerto "+app.get("port"));
 });
+
+// {
+//   "user": {
+//       "_id": "5e3860f76619150ba8bf5b8a",
+//       "name": "fernando",
+//       "surname": "mateos",
+//       "email": "fernandojosemateos.18@eusa.es",
+//       "role": "ROLE_USER",
+//       "image": "",
+//       "password": "$2a$10$2MSqRvzwcI7WN5g.lKN2Le3EL3niMjwG2YqHA0O06mR4t4gSSR1d.",
+//       "__v": 0
+//   }
+// }
