31d1b8a20d40bfde00b8ed546e55384b89e853f7
FernandoNP <fernandojosemateos.18@campuscamara.es>
2020-02-03 11:23:36 +0100

    Rutas declaradas

--- mean-concesionarios/frontend/src/app/app-routing.module.ts
+++ mean-concesionarios/frontend/src/app/app-routing.module.ts
@@ -1,11 +1,27 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./views/home/home.component";
+import { BuyersComponent } from "./views/buyers/buyers.component";
+import { CarsComponent } from "./views/cars/cars.component";
+import { VehiclesComponent } from "./views/vehicles/vehicles.component";
+import { MotocyclesComponent } from "./views/motocycles/motocycles.component";
+import { SellersComponent } from "./views/sellers/sellers.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  { path: "home", component: HomeComponent },
+  { path: "Buyers", component: BuyersComponent },
+  { path: "Sellers", component: SellersComponent },
+  { path: "Vehicles", component: VehiclesComponent },
+  { path: "Cars", component: CarsComponent },
+  { path: "Motocycles", component: MotocyclesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
