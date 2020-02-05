d04aa6366dbc192aaf4eb39b232f276da4761085
Fernando <fernandojosemateos.18@campuscamara.es>
2020-02-05 20:52:11 +0100

    token and identifier

--- mean-teachers/frontend/src/app/app.component.ts
+++ mean-teachers/frontend/src/app/app.component.ts
@@ -14,25 +14,55 @@ declare var M: any;
 export class AppComponent {
   public title = "Teacher App";
   public user: User;
-  public identity;
-  public token: String;
+  public identity=null;
+  public token=null;
   constructor(private userService: UserService) {
     this.user = new User("", "", "", "", "", "ROLE_USER", "");
   }
+  ngOnInit(){
+    this.identity=this.userService.getIdentity();
+    this.token=this.userService.getToken();
+  }
   public onSubmitLogin(form: NgForm) {
+    const params = {
+      email: form.value.email,
+      password: form.value.password,
+      gethash: true
+    };
     if (form.value.gethash) {
     } else {
       //Get data user
       this.userService.signUp(form.value).subscribe(
         response => {
-          this.identity = response;
-
-          if (!this.identity._id) {
-            M.toast({html:"Not Login"});
-          }else{
+          const identity = response["user"];
+          this.identity = identity;
+          console.log(this.token);
+          if (this.token !=null) {
+            //!this.identity._id
+            M.toast({ html: "Ok Login" });
+          } else {
             //Create element token
-            //Persis in LocalStorage
-            this.identity
+            localStorage.setItem("identity", JSON.stringify(identity));
+            //get Token
+            this.userService.signUp(params).subscribe(
+              res => {
+                console.log(this.token);
+                const token=res["token"];
+                this.token=token;
+                console.log(this.token);
+                if(this.token.length<=0){
+                  M.toast({ html: "Error Login" });
+                }else{
+                  localStorage.setItem("token",token);
+                  console.log(localStorage.getItem("token"));
+                  console.log(localStorage.getItem("identity"))
+                }
+              },
+              err => {
+                M.toast({ html: "Not Login" });
+              }
+            );
+            //Persist in LocalStorage
           }
           M.toast({ html: "Login correct" });
         },
@@ -41,5 +71,13 @@ export class AppComponent {
         }
       );
     }
+  }
+  logOut(){
+    localStorage.removeItem("identity");
+    localStorage.removeItem("token");
+    localStorage.clear();
+    this.identity=null;
+    this.token=null;
+    M.toast({html:"Logout succesfully"});
   }
 }
--- mean-teachers/frontend/src/app/services/users.services.ts
+++ mean-teachers/frontend/src/app/services/users.services.ts
@@ -5,6 +5,8 @@ import { GLOBAL } from './global';
 @Injectable()
 export class UserService{
     public url:String;
+    private identity=null;
+    private token=null;
     constructor(private http:HttpClient){
         this.url=GLOBAL.url;
     }
@@ -13,6 +15,19 @@ export class UserService{
             headers:new HttpHeaders({"Content-Type":"application/json"}),
         };
         return this.http.post(this.url+`users/login`,params,httpOptions);
-
     }
+    getIdentity(){
+        const identity=JSON.parse(localStorage.getItem("identity"));
+        if(identity!=null){
+          this.identity=identity;
+        }
+        return this.identity;
+      }
+      getToken(){
+        const token=JSON.parse(localStorage.getItem("token"));
+        if(token!=null){
+          this.token=token;
+        }
+        return this.token;
+      }
 }
