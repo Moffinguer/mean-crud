e5bbe033e261b9ac2d01cbeb303948f1fb4426ca
Moffinguer <fermago11@gmail.com>
2021-02-06 11:10:19 +0100

    second try

--- mean-teachers/server/database.js
+++ mean-teachers/server/database.js
@@ -4,6 +4,3 @@ const URI= "mongodb://localhost/mean_crud";
 mongoose.connect(URI).then(db=>console.log("Connected")).catch(err=>console.log("Not Connected"));
 
 module.exports=mongoose;
-
-
-asldhasldhasldjahskdjahskhdakjdhaksjdhakjsdhkajsdkhadskj
